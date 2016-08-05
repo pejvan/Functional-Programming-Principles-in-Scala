3874d09b7063744ff2f361be1f7b4efe18406aac
Pejvan Beigui <pejvan@gmail.com>
2016-08-05 20:19:11 +0200

    a pretty ugly implemenation of the balanced algo

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,7 @@
 package recfun
 
+//import scala.reflect.internal.TreeInfo.IsFalse
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -21,10 +23,37 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    // notes:
+    //  if the first unmatched parenthesis is the closing one, then they cannot be balanced
+    //    examples: ")hello..." or "(hello))..."
+    //  if the last char is an opening paren, it cannot be balanced
+    //    example: "hello(" or "(hello())...("
+
+    def balance(char: Char, numUnmatchedOpenParen: Int, chars: List[Char]): Boolean = {
+      if ( char != ')' && char != '(' ) {
+        if (chars.isEmpty) return numUnmatchedOpenParen == 0
+        else return balance(chars.head, numUnmatchedOpenParen, chars.tail)
+      }
+
+      if ( char == '(' ) {
+        if (chars.nonEmpty) return balance(chars.head, numUnmatchedOpenParen + 1, chars.tail)
+        else return false
+      }
+
+      if ( char == ')' ){
+        if (numUnmatchedOpenParen > 0 && chars.nonEmpty) return balance(chars.head, numUnmatchedOpenParen - 1, chars.tail)
+        else return numUnmatchedOpenParen == 1
+      }
+
+      throw new Exception("Shouldn't reach here!")
+    }
+
+    balance(chars.head, 0, chars.tail)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = 1
   }
